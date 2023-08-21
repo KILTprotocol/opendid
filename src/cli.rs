c86760c1ee8211f18dc43bd15e266e9b642aea19
Tino Rusch <tino@kilt.io>
2023-08-21 10:04:18 +0200

    add struct based program options

---
+++ src/cli.rs
@@ -0,0 +1,30 @@
+use std::path::PathBuf;
+use clap::Parser;
+
+use crate::config::Config;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    #[arg(short, long, value_name = "FILE")]
+    config: PathBuf,
+
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+}
+
+impl Cli {
+    pub fn get_config(&self) -> Result<Config, Box<dyn std::error::Error>> {
+        let config_file = std::fs::File::open(&self.config)?;
+        let config: Config = serde_yaml::from_reader(config_file)?;
+        Ok(config)
+    }
+
+    pub fn set_log_level(&self) {
+        match self.debug {
+            0 => log::set_max_level(log::LevelFilter::Info),
+            1 => log::set_max_level(log::LevelFilter::Debug),
+            _ => log::set_max_level(log::LevelFilter::Trace),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use actix_web::{
     middleware::Logger,
     web, App, HttpServer,
 };
+use clap::Parser;
 use config::{JWTConfig, SessionConfig, WellKnownDidConfig};
 use serde::{Deserialize, Serialize};
 use well_known_did_config::create_well_known_did_config;
@@ -20,6 +21,7 @@ mod routes;
 mod util;
 mod verify;
 mod well_known_did_config;
+mod cli;
 
 use crate::{
     config::{Config, CredentialRequirement},
@@ -43,104 +45,15 @@ struct AppState {
 
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    std::env::set_var("RUST_LOG", "info");
     env_logger::init();
 
-    let matches = clap::Command::new("kiltlogin")
-        .arg(
-            clap::Arg::new("session-key")
-                .long("session-key")
-                .value_name("SESSION_KEY")
-                .help("Sets the cookie session key"),
-        )
-        .arg(
-            clap::Arg::new("nacl-public-key")
-                .long("nacl-public-key")
-                .value_name("NACL_PUBLIC_KEY")
-                .help("Sets the nacl public key"),
-        )
-        .arg(
-            clap::Arg::new("nacl-secret-key")
-                .long("nacl-secret-key")
-                .value_name("NACL_SECRET_KEY")
-                .help("Sets the nacl secret key"),
-        )
-        .arg(
-            clap::Arg::new("key-uri")
-                .short('k')
-                .long("key-uri")
-                .value_name("KEY_URI")
-                .help("Sets the encryption key uri"),
-        )
-        .arg(
-            clap::Arg::new("config")
-                .short('c')
-                .long("config")
-                .value_name("CONFIG")
-                .help("Sets the config path"),
-        )
-        .get_matches();
+    let cli = cli::Cli::parse();
+    cli.set_log_level();
 
-    let config_path = matches.get_one::<String>("config");
-    let mut config = match config_path {
-        Some(config_path) => {
-            let config_file = std::fs::File::open(config_path)?;
-            let config: Config = serde_yaml::from_reader(config_file)?;
-            config
-        }
-        None => Config {
-            host: "127.0.0.1".to_string(),
-            port: 3002,
-            session_config: SessionConfig {
-                session_key: matches
-                    .get_one::<String>("session-key")
-                    .ok_or("missing session key")?
-                    .clone(),
-                key_uri: matches
-                    .get_one::<String>("key-uri")
-                    .ok_or("missing key uri")?
-                    .clone(),
-                nacl_public_key: matches
-                    .get_one::<String>("nacl-public-key")
-                    .ok_or("missing nacl public key")?
-                    .clone(),
-                nacl_secret_key: matches
-                    .get_one::<String>("nacl-secret-key")
-                    .ok_or("missing nacl secret key")?
-                    .clone(),
-            },
-            jwt_config: JWTConfig {
-                token_issuer: "dev-auth".to_string(),
-                access_token_lifetime: 60,
-                refresh_token_lifetime: 60 * 60 * 24 * 30,
-                access_token_audience: "application".to_string(),
-                refresh_token_audience: "authentication".to_string(),
-                token_secret: "secret".to_string(),
-            },
-            credential_requirements: vec![],
-            well_known_did_config: WellKnownDidConfig {
-                did: "".to_string(),
-                key_uri: "".to_string(),
-                origin: "".to_string(),
-                seed: "".to_string(),
-            },
-            base_path: "/srv".to_string(),
-            oauth_config: None,
-        },
-    };
-
-    if let Some(pk) = matches.get_one::<String>("nacl-public-key") {
-        config.session_config.nacl_public_key = pk.clone();
-    }
-    if let Some(sk) = matches.get_one::<String>("nacl-secret-key") {
-        config.session_config.nacl_secret_key = sk.clone();
-    }
-    if let Some(key_uri) = matches.get_one::<String>("key-uri") {
-        config.session_config.key_uri = key_uri.clone();
-    }
+    let config = cli.get_config()?;
 
     let state = AppState {
-        app_name: "kiltlogin".to_string(),
+        app_name: "simple-auth-relay-app".to_string(),
         encryption_key_uri: config.session_config.key_uri.to_string(),
         public_key: config.get_nacl_public_key()?,
         secret_key: config.get_nacl_secret_key()?,
@@ -166,7 +79,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     .cookie_http_only(true)
                     .cookie_same_site(SameSite::Lax)
                     .cookie_secure(false) // TODO: set to true when using HTTPS!
-                    .cookie_name("kiltlogin".to_string())
+                    .cookie_name("sara".to_string())
                     .session_lifecycle(
                         PersistentSession::default().session_ttl(Duration::seconds(60)),
                     )
