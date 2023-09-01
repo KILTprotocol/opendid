7cc3fe0a221f05b9ec93e0714ab0c469f5cd6895
Tino Rusch <tino@kilt.io>
2023-09-01 10:23:04 +0200

    preserve locally configured clients when using etcd + add some comments to the new code

--- src/config_updater.rs
+++ src/config_updater.rs
@@ -1,23 +1,30 @@
-use std::{collections::HashMap, sync::Mutex};
-
-use actix_web::web;
-
 use crate::{config::EtcdConfig, routes::error::Error, AppState};
+use actix_web::web;
+use std::sync::Mutex;
 
+// ConfigUpdater is responsible for fetching client configurations from etcd and
+// updating the app state with the latest configurations.
 pub struct ConfigUpdater {
     app_state: web::Data<Mutex<AppState>>,
     cli: etcd_client::Client,
 }
 
 impl ConfigUpdater {
+    // new creates a new ConfigUpdater.
+    // it takes a protected reference to the app state and the etcd config.
     pub async fn new(
         app_state: web::Data<Mutex<AppState>>,
         config: EtcdConfig,
     ) -> Result<Self, Box<dyn std::error::Error>> {
+        // first we need to assemble the etcd client options
         let mut opts = etcd_client::ConnectOptions::new();
+
+        // we support user/password
         if config.user.is_some() && config.password.is_some() {
             opts = opts.with_user(config.user.unwrap(), config.password.unwrap());
         }
+
+        // we support tls
         if config.tls_ca_cert.is_some()
             || config.tls_domain_name.is_some()
             || (config.tls_client_cert.is_some() && config.tls_client_key.is_some())
@@ -39,15 +46,21 @@ impl ConfigUpdater {
             opts = opts.with_tls(tls_options);
         }
 
+        // now we can create the client
         let cli = etcd_client::Client::connect(config.endpoints, Some(opts)).await?;
+
         Ok(Self { app_state, cli })
     }
 
+    // read_initial_config reads the initial client configurations from etcd.
     pub async fn read_initial_config(&mut self) -> Result<(), Box<dyn std::error::Error + '_>> {
+        // we start from the client configs in the current app state
         let mut client_configs = {
             let app_state = self.app_state.lock()?;
             app_state.client_configs.clone()
         };
+
+        // we fetch all the client configs from etcd prefixed by "/sara/clients"
         let resp = self
             .cli
             .get(
@@ -55,6 +68,8 @@ impl ConfigUpdater {
                 Some(etcd_client::GetOptions::new().with_prefix()),
             )
             .await?;
+
+        // we iterate over the results and parse the client configs
         for kv in resp.kvs() {
             let client_id = kv
                 .key_str()?
@@ -65,17 +80,22 @@ impl ConfigUpdater {
             log::info!("Loaded client config for {}", &client_id);
             client_configs.insert(client_id, client_config);
         }
+
+        // we update the app state with the latest client configs
         let mut app_state = self.app_state.lock()?;
         app_state.client_configs = client_configs.clone();
         Ok(())
     }
 
+    // watch_for_updates watches for changes in the client configurations.
     pub async fn watch_for_updates(&mut self) -> Result<(), Box<dyn std::error::Error + '_>> {
+        // we start from the client configs in the current app state
         let mut client_configs = {
             let app_state = self.app_state.lock()?;
             app_state.client_configs.clone()
         };
-        // watch for changes
+
+        // we watch for changes in "/sara/clients"
         let (_, mut stream) = self
             .cli
             .watch(
@@ -84,8 +104,11 @@ impl ConfigUpdater {
             )
             .await?;
         log::info!("Watching for changes in /sara/clients");
+
+        // we iterate over the stream of events
         while let Some(resp) = stream.message().await? {
             for event in resp.events() {
+                // we parse the client id from the event key
                 let client_id = event
                     .kv()
                     .ok_or(Error::OauthInvalidClientId)?
@@ -95,6 +118,7 @@ impl ConfigUpdater {
                     .to_string();
                 log::info!("Received event for client {}", client_id);
                 match event.event_type() {
+                    // if the event is a PUT, we parse the client config and add it to the map
                     etcd_client::EventType::Put => {
                         log::info!("Received PUT event for client {}", client_id);
                         let client_config: crate::config::ClientConfig = serde_json::from_slice(
@@ -102,12 +126,15 @@ impl ConfigUpdater {
                         )?;
                         client_configs.insert(client_id, client_config);
                     }
+                    // if the event is a DELETE, we remove the client config from the map
                     etcd_client::EventType::Delete => {
                         log::info!("Received DELETE event for client {}", client_id);
                         client_configs.remove(&client_id);
                     }
                 }
             }
+
+            // we update the app state with the latest client configs
             let mut app_state = self.app_state.lock()?;
             app_state.client_configs = client_configs.clone();
         }
