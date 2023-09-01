a283f9113c73aa88604454f498a9def9a49e8c3f
Tino Rusch <tino@kilt.io>
2023-09-01 11:30:04 +0200

    make client_id configurable in demo page to better test etcd

--- demo-project/demo-frontend/login.js
+++ demo-project/demo-frontend/login.js
@@ -1,7 +1,8 @@
 // add click listener to button to trigger login
 const loginButton = document.getElementById('login');
 loginButton.addEventListener('click', () => {
+    const clientId = document.getElementById('client-id').value;
     const nonce = document.cookie.split('; ').find(row => row.startsWith('nonce')).split('=')[1];
     const state = document.cookie.split('; ').find(row => row.startsWith('state')).split('=')[1];
-    window.location.href = `http://localhost:3001/api/v1/authorize?response_type=id_token&client_id=example-client&redirect_uri=http://localhost:1606/callback.html&scope=openid&state=${state}&nonce=${nonce}`;
+    window.location.href = `http://localhost:3001/api/v1/authorize?response_type=id_token&client_id=${clientId}&redirect_uri=http://localhost:1606/callback.html&scope=openid&state=${state}&nonce=${nonce}`;
 });
