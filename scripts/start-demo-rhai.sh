405362ece509ab20011523c7792b5accb43de611
Tino Rusch <tino@kilt.io>
2023-09-07 15:32:08 +0200

    also do the openid rename in the rhai demo script

--- scripts/start-demo-rhai.sh
+++ scripts/start-demo-rhai.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # create a pod to hold backend and frontend
-podman pod create --replace -p 1606:1606 -p 3001:3001 sara-test
+podman pod create --replace -p 1606:1606 -p 3001:3001 -n openid-test
 
 # start the simple auth relay app
 podman run -d --rm \
-  --pod sara-test \
-  --name sara-backend \
+  --pod openid-test \
+  --name openid-backend \
   -e RUST_LOG=info \
   -v $(pwd)/config.yaml:/app/config.yaml \
   -v $(pwd)/scripts/example-checks:/app/checks \
@@ -14,7 +14,7 @@ podman run -d --rm \
 
 # start the example client frontend
 podman run -d --rm \
-  --pod sara-test \
+  --pod openid-test \
   --name demo-frontend \
   quay.io/kilt/simple-auth-relay-app-demo
 
