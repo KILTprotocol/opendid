c45d8dfae3004e2750b1ca57d467041f4046fa31
Skedley <48058984+Dudleyneedham@users.noreply.github.com>
2023-10-18 10:44:16 +0200

    feat: updating the typescript scripts (#26)
    * feat: cleaning up the script and adding did seed as arg for script
    
    * feat: removal of the gen key as not used
    
    * feat: updating git ignore
    
    * feat: updating git ignore
    
    * feat: updating git ignore
    
    * fix: removal of yarn lock
    
    * feat: updating the endpint
    
    * feat: updating the linting
    
    * feat: adding new prettier rules
    
    * feat: fixing the script
    
    * style: adding new line at the end
    
    * feat: removed second arg and made sr25519 a constant
    
    * feat: updating and cleaning up script and making utils
    
    * feat: updating utils
    
    * feat: cleaning up the key generation
    
    * feat: removal of the console log

---
+++ scripts/gen-did/utils/utils.ts
@@ -0,0 +1,93 @@
+import * as Kilt from '@kiltprotocol/sdk-js'
+
+import * as fs from 'fs'
+import * as fsPromise from 'fs/promises'
+
+import { u8aToHex } from '@polkadot/util'
+
+import {
+    blake2AsU8a,
+    keyExtractPath,
+    keyFromPath,
+    mnemonicGenerate,
+    mnemonicToMiniSecret,
+    sr25519PairFromSeed,
+} from '@polkadot/util-crypto'
+
+export interface DidSecrets {
+    did: string
+    authentication: {
+        pubKey: string
+        seed: string
+    }
+    attestation: {
+        pubKey: string
+        seed: string
+    }
+    keyAgreement: {
+        pubKey: string
+        seed: string
+        privKey: string
+    }
+}
+
+export async function checkAndWriteFile(didSecrets: object, filename: string) {
+    if (fs.existsSync(filename)) {
+        return console.error(`files ${filename} exist already`)
+    }
+    await fsPromise.writeFile(filename, JSON.stringify(didSecrets, null, 2))
+    return console.debug(`Wrote ${filename}`)
+}
+
+export const signingKeyType = 'sr25519'
+
+export function generateKeyAgreement(mnemonic: string): Kilt.KiltEncryptionKeypair {
+    const secretKeyPair = sr25519PairFromSeed(mnemonicToMiniSecret(mnemonic))
+    const { path } = keyExtractPath('//did//keyAgreement//0')
+    const { secretKey } = keyFromPath(secretKeyPair, path, signingKeyType)
+    return Kilt.Utils.Crypto.makeEncryptionKeypairFromSeed(blake2AsU8a(secretKey))
+}
+
+export function generateKeypairs(mnemonic = mnemonicGenerate()): {
+    baseMnemonic: string
+    authentication: Kilt.KiltKeyringPair
+    keyAgreement: Kilt.KiltEncryptionKeypair
+    assertionMethod: Kilt.KiltKeyringPair
+} {
+    const authentication = Kilt.Utils.Crypto.makeKeypairFromUri(mnemonic + '//did//0')
+
+    const assertionMethod = Kilt.Utils.Crypto.makeKeypairFromUri(mnemonic + '//did//assertion//0')
+
+    const keyAgreement = generateKeyAgreement(mnemonic)
+
+    return {
+        baseMnemonic: mnemonic,
+        authentication: authentication,
+        keyAgreement: keyAgreement,
+        assertionMethod: assertionMethod,
+    }
+}
+
+export function exportKeypairs(keypairs: {
+    baseMnemonic: string
+    authentication: Kilt.KiltKeyringPair
+    keyAgreement: Kilt.KiltEncryptionKeypair
+    assertionMethod: Kilt.KiltKeyringPair
+}): DidSecrets {
+    return {
+        did: `did:kilt:${keypairs.authentication.address}`,
+        authentication: {
+            pubKey: keypairs.authentication.address,
+            seed: keypairs.baseMnemonic + '//did//0',
+        },
+        attestation: {
+            pubKey: keypairs.assertionMethod.address,
+            seed: keypairs.baseMnemonic + '//did//assertion//0',
+        },
+        keyAgreement: {
+            pubKey: u8aToHex(keypairs.keyAgreement.publicKey),
+            seed: keypairs.baseMnemonic + '//did//keyAgreement//0',
+            privKey: u8aToHex(keypairs.keyAgreement.secretKey),
+        },
+    }
+}
