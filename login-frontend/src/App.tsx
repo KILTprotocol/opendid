40d3577a440e1c03ddc5b0bb6a1f874c40db1bb0
Adel Golghalyani <ad96el@gmail.com>
2023-11-06 14:59:33 +0100

    frontend

--- login-frontend/src/App.tsx
+++ login-frontend/src/App.tsx
@@ -97,7 +97,7 @@ export function App() {
       // jwt parts
       const nbf = Math.floor(Date.now() / 1000);
       const exp = nbf + 60;
-      const kid = `${did}${didDocument?.document?.authentication[0]}`;
+      const kid = `${did}${didDocument?.document?.authentication[0].id}`;
       const kty = didDocument?.document?.authentication[0].type;
       // Not completely standard, but SR25519 is not commonly used for JWTs.
       const header = { alg: 'EdDSA', typ: 'JWT', crv: 'Ed25519', kid, kty };
