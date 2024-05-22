a266993a71224c4996cdd56e59df0102147359e7
Abdulrahim Al Methiab <abdul@kilt.io>
2024-05-22 13:43:22 +0200

    format main.ts

--- demo-project/main.ts
+++ demo-project/main.ts
@@ -11,11 +11,11 @@ const app = express()
 app.use(bodyParser.json())
 
 declare global {
-  namespace Express {
-    interface Request {
-      auth?: any;
+    namespace Express {
+        interface Request {
+            auth?: any;
+        }
     }
-  }
 }
 
 const port = 1606
@@ -25,8 +25,8 @@ const tokenSecret = process.env.TOKEN_SECRET ?? 'super-secret-jwt-secret'
 
 // Allow CORS from http://localhost:3001
 app.use(cors({
-  origin: 'http://localhost:3001',
-  credentials: true
+    origin: 'http://localhost:3001',
+    credentials: true
 }))
 
 // Parse cookies
@@ -34,55 +34,55 @@ app.use(cookieParser());
 
 // redirect to login by default
 app.get('/', (req, res) => {
-  res.redirect('/login.html')
+    res.redirect('/login.html')
 })
 
 // Serve login page
 // This also sets a random nonce and state cookie that is used for constructing the openid connect request
 app.get('/login.html', (req, res) => {
-  const nonce = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
-  const state = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
-  res.cookie('nonce', nonce, { maxAge: 900000 });
-  res.cookie('state', state, { maxAge: 900000 });
-  res.sendFile('/srv/demo-frontend/login.html')
+    const nonce = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+    const state = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+    res.cookie('nonce', nonce, { maxAge: 900000 });
+    res.cookie('state', state, { maxAge: 900000 });
+    res.sendFile('/srv/demo-frontend/login.html')
 })
 
 // This is a protected endpoint that requires a valid JWT token
 app.get('/protected', jwt({ secret: tokenSecret, algorithms: ['HS256'] }), (req, res) => {
-  let token = req.auth;
-  // check that token.nonce matches the nonce cookie
-  if (token.nonce !== req.cookies.nonce) {
-    res.status(401).send('Invalid nonce')
-    return
-  }
-  const name = getNameFromToken(token)
-  res.send('Hello from protected route ' + name)
+    let token = req.auth;
+    // check that token.nonce matches the nonce cookie
+    if (token.nonce !== req.cookies.nonce) {
+        res.status(401).send('Invalid nonce')
+        return
+    }
+    const name = getNameFromToken(token)
+    res.send('Hello from protected route ' + name)
 })
 
 // This is a protected endpoint that requires a valid Authorization Code.
 app.post('/protected/AuthorizationCode', async (req, res) => {
-  const codeRequestBody = {
-    code: req.body.auth_code,
-    grant_type: "authorization_code",
-    redirect_uri: "http://localhost:1606/callback.html",
-    client_id: "example-client"
-  }
-  let response: Response = await fetch("http://localhost:3001/api/v1/token", {
-    method: "POST",
-    headers:
-      { "Content-Type": "application/x-www-form-urlencoded" },
-    body: qs.stringify(codeRequestBody)
-  }
-  );
-  let idToken = (await response.json()).id_token;
+    const codeRequestBody = {
+        code: req.body.auth_code,
+        grant_type: "authorization_code",
+        redirect_uri: "http://localhost:1606/callback.html",
+        client_id: "example-client"
+    }
+    let response: Response = await fetch("http://localhost:3001/api/v1/token", {
+        method: "POST",
+        headers:
+            { "Content-Type": "application/x-www-form-urlencoded" },
+        body: qs.stringify(codeRequestBody)
+    }
+    );
+    let idToken = (await response.json()).id_token;
 
-  let decodedToken: any = jsonwebtoken.verify(idToken, tokenSecret);
-  if (decodedToken.nonce !== req.cookies.nonce) {
-    res.status(401).send('Invalid nonce')
-    return
-  }
-  const name = getNameFromToken(decodedToken)
-  res.send('Hello from protected route ' + name)
+    let decodedToken: any = jsonwebtoken.verify(idToken, tokenSecret);
+    if (decodedToken.nonce !== req.cookies.nonce) {
+        res.status(401).send('Invalid nonce')
+        return
+    }
+    const name = getNameFromToken(decodedToken)
+    res.send('Hello from protected route ' + name)
 })
 
 
@@ -91,10 +91,10 @@ app.use('/', express.static('demo-frontend'))
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
 
 // use the token to get the user's web3 name, if not present use the users DID
 function getNameFromToken(token: any) {
-  return token.w3n ? `w3n:${token.w3n}` : token.sub
+    return token.w3n ? `w3n:${token.w3n}` : token.sub
 }
