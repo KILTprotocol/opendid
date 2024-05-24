abfe3f3ed94265f9edd84a1ca5852f82aa4dc07a
Abdulrahim Al Methiab <abdul@kilt.io>
2024-05-24 10:44:45 +0200

    improve demo frontend button listeners

--- demo-project/demo-frontend/login.js
+++ demo-project/demo-frontend/login.js
@@ -1,29 +1,14 @@
-// add click listeners to login buttons.
 const loginButtonImplicit = document.getElementById('login-implicit');
-loginButtonImplicit.addEventListener('click', () => {
-  const clientId = getClientIdElement();
-  const nonce = getNonce();
-  const state = getState();
-  window.location.href = `http://localhost:3001/api/v1/authorize?response_type=id_token&client_id=${clientId}&redirect_uri=http://localhost:1606/callback.html&scope=openid&state=${state}&nonce=${nonce}`;
-});
+authorizeOnButtonClick(loginButtonImplicit, "id_token")
 
 const loginButtonAuthCode = document.getElementById('login-authorization-code');
-loginButtonAuthCode.addEventListener('click', () => {
-  const clientId = getClientIdElement();
-  const nonce = getNonce();
-  const state = getState();
-  window.location.href = `http://localhost:3001/api/v1/authorize?response_type=code&client_id=${clientId}&redirect_uri=http://localhost:1606/callback.html&scope=openid&state=${state}&nonce=${nonce}`;
-});
+authorizeOnButtonClick(loginButtonAuthCode, "code");
 
-function getState() {
-    return document.cookie.split('; ').find(row => row.startsWith('state')).split('=')[1];
+function authorizeOnButtonClick(button, responseType){
+    button.addEventListener('click', () => {
+      const clientId = document.getElementById('client-id').value;
+      const nonce = document.cookie.split('; ').find(row => row.startsWith('nonce')).split('=')[1];
+      const state = document.cookie.split('; ').find(row => row.startsWith('state')).split('=')[1];
+      window.location.href = `http://localhost:3001/api/v1/authorize?response_type=${responseType}&client_id=${clientId}&redirect_uri=http://localhost:1606/callback.html&scope=openid&state=${state}&nonce=${nonce}`;
+    });
 }
-
-function getNonce() {
-    return document.cookie.split('; ').find(row => row.startsWith('nonce')).split('=')[1];
-}
-
-function getClientIdElement() {
-    return document.getElementById('client-id').value;
-}
-
