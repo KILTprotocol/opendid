9113f40b24b09bce9cf8ce2c789bb3fdaa715381
Abdulrahim Al Methiab <abdul@kilt.io>
2024-06-07 09:32:31 +0200

    add prettier config

--- tests/eslint.config.js
+++ tests/eslint.config.js
@@ -1,7 +1,9 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-);
+  eslintPluginPrettierRecommended,
+)
