0e45d28d5cf2a7e0425183bfb50d9689570cfe8f
Abdulrahim Al Methiab <abdul@kilt.io>
2024-06-27 14:07:58 +0200

    improvements

--- tests/tests/error_messages.test.ts
+++ tests/tests/error_messages.test.ts
@@ -7,14 +7,16 @@ import { describe, it } from 'vitest'
 
 const authorizeUrl = new URL('api/v1/authorize', OPENDID_URL)
 
+const ERROR_DESCRIPTION = 'error_description'
+const STATE = 'state'
 //Error Messages from ./routes/error.rs file
-const authInvalidClientId = "Invalid client_id"
-const responseType = "Invalid response_type"
-const invalidNonce = "Invalid nonce"
-const authInvalidRedirectUri = "Invalid redirect_uri"
+const ERR_MSG_INVALID_CLIENT_ID = 'Invalid client_id'
+const ERR_MSG_INVALID_RESPONSE_TYPE = 'Invalid response_type'
+const ERR_MSG_INVALID_NONCE = 'Invalid nonce'
+const ERR_MSG_INVALID_REDIRECT_URI = 'Invalid redirect_uri'
 
 describe('Authorize endpoint', async () => {
-  it('should return error if client_id is given but has invaldi config', async () => {
+  it('should return error if client_id is given is invalid', async () => {
     const reqParams = {
       client_id: 'invalid_client',
       redirect_uri: REDIRECT_URI,
@@ -29,21 +31,20 @@ describe('Authorize endpoint', async () => {
       maxRedirects: 0,
       validateStatus: () => true,
     })
-    
+
     expect(response.status).toBe(302)
     const locationUrl = new URL(response.headers.location)
 
-    const errorDescription = locationUrl.searchParams.get('error_description')
-    expect(errorDescription).toBe(authInvalidClientId)
-    
-    const state = locationUrl.searchParams.get('state')
+    const errorDescription = locationUrl.searchParams.get(ERROR_DESCRIPTION)
+    expect(errorDescription).toBe(ERR_MSG_INVALID_CLIENT_ID)
+
+    const state = locationUrl.searchParams.get(STATE)
     expect(state).toBe(reqParams.state)
-  }
-)
+  })
 })
 
 describe('Authorize endpoint', async () => {
-  it('should return error if client_id is not given', async () => {
+  it('should return error if client_id is missing', async () => {
     const reqParams = {
       redirect_uri: REDIRECT_URI,
       response_type: 'code',
@@ -57,51 +58,47 @@ describe('Authorize endpoint', async () => {
       maxRedirects: 0,
       validateStatus: () => true,
     })
-    
+
     expect(response.status).toBe(302)
     const locationUrl = new URL(response.headers.location)
 
-    const errorDescription = locationUrl.searchParams.get('error_description')
-    expect(errorDescription).toBe(authInvalidClientId)
+    const errorDescription = locationUrl.searchParams.get(ERROR_DESCRIPTION)
+    expect(errorDescription).toBe(ERR_MSG_INVALID_CLIENT_ID)
 
-    const state = locationUrl.searchParams.get('state')
+    const state = locationUrl.searchParams.get(STATE)
     expect(state).toBe(reqParams.state)
-  }
-)
+  })
 })
-
 
 describe('Authorize endpoint', async () => {
   it('should return error if redirect uri is invalid', async () => {
     const reqParams = {
       client_id: CLIENT_ID,
-      redirect_uri: 'http://localhost:16006/',
+      redirect_uri: 'http://example.com/',
       response_type: 'id_token',
       scope: 'opendid',
       state: TestState.STATE,
-      nonce: ""
+      nonce: '',
     }
     const urlWithParams = addQueryParamsToUrl(authorizeUrl, reqParams)
-    // Don't redirect, and don't throw on status 302.
     const response = await axios.get(urlWithParams.toString(), {
       maxRedirects: 0,
       validateStatus: () => true,
     })
-    
+
     expect(response.status).toBe(302)
     const locationUrl = new URL(response.headers.location)
 
-    const errorDescription = locationUrl.searchParams.get('error_description')
-    expect(errorDescription).toBe(authInvalidRedirectUri)
+    const errorDescription = locationUrl.searchParams.get(ERROR_DESCRIPTION)
+    expect(errorDescription).toBe(ERR_MSG_INVALID_REDIRECT_URI)
 
-    const state = locationUrl.searchParams.get('state')
+    const state = locationUrl.searchParams.get(STATE)
     expect(state).toBe(reqParams.state)
-  }
-)
+  })
 })
 
 describe('Authorize endpoint', async () => {
-  it('should return error if response type is not given', async () => {
+  it('should return error if response type is missing', async () => {
     const reqParams = {
       client_id: CLIENT_ID,
       redirect_uri: REDIRECT_URI,
@@ -110,22 +107,20 @@ describe('Authorize endpoint', async () => {
       nonce: TestState.NONCE,
     }
     const urlWithParams = addQueryParamsToUrl(authorizeUrl, reqParams)
-    // Don't redirect, and don't throw on status 302.
     const response = await axios.get(urlWithParams.toString(), {
       maxRedirects: 0,
       validateStatus: () => true,
     })
-    
+
     expect(response.status).toBe(302)
     const locationUrl = new URL(response.headers.location)
 
-    const errorDescription = locationUrl.searchParams.get('error_description')
-    expect(errorDescription).toBe(responseType)
+    const errorDescription = locationUrl.searchParams.get(ERROR_DESCRIPTION)
+    expect(errorDescription).toBe(ERR_MSG_INVALID_RESPONSE_TYPE)
 
-    const state = locationUrl.searchParams.get('state')
+    const state = locationUrl.searchParams.get(STATE)
     expect(state).toBe(reqParams.state)
-  }
-)
+  })
 })
 
 describe('Authorize endpoint', async () => {
@@ -138,21 +133,19 @@ describe('Authorize endpoint', async () => {
       state: TestState.STATE,
     }
     const urlWithParams = addQueryParamsToUrl(authorizeUrl, reqParams)
-    // Don't redirect, and don't throw on status 302.
     const response = await axios.get(urlWithParams.toString(), {
       maxRedirects: 0,
       validateStatus: () => true,
     })
-    
+
     expect(response.status).toBe(302)
     const locationUrl = new URL(response.headers.location)
 
-    const errorDescription = locationUrl.searchParams.get('error_description')
-    expect(errorDescription).toBe(invalidNonce)
+    const errorDescription = locationUrl.searchParams.get(ERROR_DESCRIPTION)
+    expect(errorDescription).toBe(ERR_MSG_INVALID_NONCE)
 
-    const state = locationUrl.searchParams.get('state')
+    const state = locationUrl.searchParams.get(STATE)
     expect(state).toBe(reqParams.state)
-
-  }
-)
+  })
 })
+
